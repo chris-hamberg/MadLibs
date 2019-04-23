1e15934a8f9cff68651dd30c344fe78f1f479dfa
Chris Hamberg <chris.hamberg@programmer.net>
2019-04-22 22:57:06 -0400

    java project

--- src/main/java/com/hamberg/madlibs/MadLibs.java
+++ src/main/java/com/hamberg/madlibs/MadLibs.java
@@ -6,10 +6,10 @@ public class MadLibs {
 	
 	public static void main(String[] args) {
 		// Simple madlib prototype
-	    Procedures procs = new Procedures();
+		Procedures procs = new Procedures();
 	    procs.prompt();
-		System.out.format("MadLib: \n" +
-				"The %s farmer %s %s %s on his farm in " +
+	    System.out.format("MadLib: \n" +
+	    		"The %s farmer %s %s %s on his farm in " +
 				"%s before he %s %s with his %s %s.", 
 				procs.getUdata());
 	}
